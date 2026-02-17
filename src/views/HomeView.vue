da6ad9955e21b4d00bfdcb1a3ba2cd275cb78320
JorgeLNJunior <jorgelnjunior@gmail.com>
2026-02-17 13:49:40 -0300

    chore: add a home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ResumeForm from '@/components/resumeForm.vue'
+import ResumeTemplate from '@/components/resumeTemplate.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-2">
+    <section class="m-6" aria-label="Editor de Currículo">
+      <ResumeForm />
+    </section>
+    <section aria-label="Pré-visualização do Currículo">
+      <ResumeTemplate />
+    </section>
+  </div>
+</template>
