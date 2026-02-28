1336a849f074c03e12591fddcc7213708c8cb040
Jorge Junior <jorgelnjunior@gmail.com>
2026-02-28 14:42:56 -0300

    feat: add a url to the repository at navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { Github } from 'lucide-vue-next'
+
+import { buttonVariants } from '@/components/ui/button'
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,15 +10,16 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
+import { cn } from '@/lib/utils'
 </script>
 
 <template>
   <header
     class="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60"
   >
-    <div class="container flex h-14 items-center px-4">
+    <div class="flex h-14 items-center px-6 justify-between">
       <div class="mr-4 flex">
-        <RouterLink class="mr-6 flex items-center space-x-2" to="/">
+        <RouterLink class="mr-6 flex items-center space-x-3" to="/">
           <img src="/logo.png" alt="vaggo Logo" class="h-8 w-8 object-contain" />
           <span class="hidden font-bold sm:inline-block">Vaggo</span>
         </RouterLink>
@@ -46,6 +50,23 @@ import {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
+      <a
+        class="end-0"
+        href="https://github.com/JorgeLNJunior/vaggo"
+        target="_blank"
+        rel="noreferrer"
+        :class="
+          cn(
+            buttonVariants({
+              variant: 'ghost',
+              size: 'icon',
+            }),
+          )
+        "
+      >
+        <Github class="h-4 w-4" />
+        <span class="sr-only">GitHub</span>
+      </a>
     </div>
   </header>
 </template>
